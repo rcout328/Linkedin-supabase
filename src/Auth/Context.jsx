77c6f3cb4c5cfe1d3d53d3200b684f59ecdbd69c
rcout328 <solovpxoffical@gmail.com>
2024-02-19 20:51:44 +0530

    commit

--- src/Auth/Context.jsx
+++ src/Auth/Context.jsx
@@ -2,6 +2,7 @@ import { createContext } from "react";
 import { useState } from "react";
 export const LoginContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 export const LoginProvider = ({ children }) => {
   const [session, setSession] = useState(null);
 
